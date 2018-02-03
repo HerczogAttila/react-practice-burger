e13e748f6a7d126ea28c5e3c7f9446d338221c7e
HerczogAttila <herczogattil@gmail.com>
2018-02-03 23:13:35 +0100

    117. Connecting State to Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,24 +4,50 @@ import Aux from '../../hoc/Aux_win'
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.4,
+  cheese: 1.3,
+  meat: 0.7
+};
+
 class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
     this.state = {
       ingredients: {
-        salad: 1,
-        bacon: 1,
-        cheese: 1,
-        meat: 1
-      }
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+      },
+      totalPrice: 4
     }
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredient = {...this.state.ingredients};
+    updatedIngredient[type] = updatedCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredient})
+  }
+
+  removeIngredientHandler = (type) => {
+  
   }
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+        />
       </Aux>
     );
   }
