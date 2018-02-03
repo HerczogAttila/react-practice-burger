cfb8d7c721106888bfe12da6c1981d56dde74269
HerczogAttila <herczogattil@gmail.com>
2018-02-03 23:31:24 +0100

    118. Removing Ingredients Safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,10 +17,11 @@ const buildControls = (props) => (
         key={control.label}
         label={control.label}
         added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+        disabled={props.disabled[control.type]}
       />
     ))}
   </div>
 )
-
 
 export default buildControls;
