dc8397dcb00e93dcf2cc354107d935781181ae1f
Herczog Attila <aherczog@precognox.com>
2018-02-07 09:36:49 +0100

    222. Handling User Input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -91,6 +91,14 @@ class ContactData extends Component {
     console.log(this.props.ingredients);
   };
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {...this.state.orderForm};
+    const updatedFormElement = {...updatedOrderForm[inputIdentifier]};
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -108,6 +116,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
         <Button buttonType="Success" clicked={this.orderHandler}>Order</Button>
