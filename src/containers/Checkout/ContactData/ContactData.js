ce5a4af5a04cd6ca05096f0e357d397ae1c34c85
Herczog Attila <aherczog@precognox.com>
2018-02-07 10:43:00 +0100

    231. Wrap Up

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,6 +85,7 @@ class ContactData extends Component {
           ]
         },
         value: 'cheapest',
+        validation: {},
         valid: true
       }
     },
@@ -94,6 +95,9 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+    if (!rules) {
+      return true;
+    }
 
     if(rules.required) {
       isValid = value.trim() !== '' && isValid;
