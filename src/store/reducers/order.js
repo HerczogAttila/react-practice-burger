48cf6e1f62ba5babe8822339bc5b21e726ce1330
Herczog Attila <aherczog@precognox.com>
2018-02-09 14:46:03 +0100

    299. Refactoring Reducers Continued

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,6 +7,29 @@ const initialState = {
   purchased: false
 };
 
+const purchaseBurgerSuccess = (state, action) => {
+  const newOrder = updateObject(action.orderData, {id: action.orderId});
+  return updateObject(state, {
+    loading: false,
+    purchased: true,
+    orders: state.orders.concat(newOrder)
+  });
+};
+
+const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    orders: action.orders
+  });
+};
+
+const fetchOrdersFail = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    error: action.error
+  });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
@@ -14,33 +37,17 @@ const reducer = (state = initialState, action) => {
     case actionTypes.PURCHASE_BURGER_START:
       return updateObject(state, {loading: true});
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = updateObject(action.orderData, {id: action.orderId});
-      return updateObject(state, {
-        loading: false,
-        purchased: true,
-        orders: state.orders.concat(newOrder)
-      });
+      return purchaseBurgerSuccess(state, action);
     case actionTypes.PURCHASE_BURGER_FAIL:
       return updateObject(state, {loading: false});
-    case actionTypes.FETCH_ORDERS_INIT: {
+    case actionTypes.FETCH_ORDERS_INIT:
       return updateObject(state, {loading: true});
-    }
-    case actionTypes.FETCH_ORDERS_SUCCESS: {
-      return updateObject(state, {
-        loading: false,
-        orders: action.orders
-      });
-    }
-    case actionTypes.FETCH_ORDERS_FAIL: {
-      return updateObject(state, {
-        loading: false,
-        error: action.error
-      });
-    }
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return fetchOrdersSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return fetchOrdersFail(state, action);
     default:
-      return {
-        ...state
-      }
+      return {...state}
   }
 };
 
