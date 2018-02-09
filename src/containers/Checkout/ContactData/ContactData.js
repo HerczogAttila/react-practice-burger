de889ef78f66bf0a7d60e60252231d1e653b63b2
Herczog Attila <aherczog@precognox.com>
2018-02-09 11:26:22 +0100

    291. Working on Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
         valid: true
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   };
 
   checkValidity(value, rules) {
@@ -174,7 +173,7 @@ class ContactData extends Component {
         <Button buttonType="Success" disabled={!this.state.formIsValid} clicked={this.orderHandler}>Order</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner/>
     }
 
@@ -190,15 +189,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ingredients: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
   }
 };
 
-export default connect(mapStateToProps)
-withErrorHandler(ContactData, axios);
+export default connect(mapStateToProps, mapDispatchToProps())(withErrorHandler(ContactData, axios));
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -6,18 +6,25 @@ export const purchaseBurgerSuccess = (id, orderData) => {
     type: actionTypes.PURCHASE_BURGER_SUCCESS,
     orderId: id,
     orderData: orderData
-  }
+  };
 };
 
 export const purchaseBurgerFail = (error) => {
   return {
     type: actionTypes.PURCHASE_BURGER_FAIL,
     error: error
+  };
+};
+
+export const purchaseBurgerStart = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_START
   }
 };
 
-export const purchaseBurgerStart = (orderData) => {
+export const purchaseBurger = (orderData) => {
   return dispatch => {
+    dispatch(purchaseBurgerStart());
     axios.post('/orders.json', orderData)
       .then(response => {
         dispatch(purchaseBurgerSuccess(response.data, orderData));
