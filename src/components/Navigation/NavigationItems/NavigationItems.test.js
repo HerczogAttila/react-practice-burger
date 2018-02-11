2c2a521dcbc734c7266d2ce492ea76320fca5a10
Herczog Attila <herczogattil@gmail.com>
2018-02-11 14:57:24 +0100

    341. Testing Containers

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import {configure, shallow} from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
   let wrapper;
@@ -20,12 +20,12 @@ describe('<NavigationItems />', () => {
   });
 
   it('should render three <NavigationItem /> elements if authenticated', () => {
-    wrapper.setProps({isAuthenticated: true});
+    wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 
   it('should an exact logout button', () => {
-    wrapper.setProps({isAuthenticated: true});
+    wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
   });
 });
---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import BurgerBuilder from './BurgerBuilder';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import { createStore, combineReducers } from 'redux';
+import burgerBuilderReducer from '../../store/reducers/burgerBuilder';
+import orderReducer from '../../store/reducers/order';
+import authReducer from '../../store/reducers/auth';
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+  auth: authReducer
+});
+
+const store = createStore(rootReducer);
+
+configure({ adapter: new Adapter() });
+
+describe('<BurgerBuilder />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<BurgerBuilder store={store} />);
+  });
+
+  it('should render <BuildControls /> when receiving ingredients', () => {
+    wrapper.setProps({ ingredients: {salad: 0}});
+    expect(wrapper.find(BuildControls)).toHaveLength(0);
+  });
+});
