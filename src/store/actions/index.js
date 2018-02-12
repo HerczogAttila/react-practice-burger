621446dbbf768ed5cf202fc1db41256355b6d0dc
Herczog Attila <aherczog@precognox.com>
2018-02-12 12:14:33 +0100

    405. Moving the Orders Side Effects into Sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,15 @@
 export { addIngredient, removeIngredient, initIngredients, setIngredients, fetchIngredientsFailed } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerSuccess,
+  purchaseBurgerFail,
+  fetchOrdersSuccess,
+  fetchOrdersStart,
+  fetchOrdersFail
+} from './order';
 export {
   auth,
   logout,
